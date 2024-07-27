87b0791939ee4354feb058ddadce4d0daf7dfb0e
tolya199720 <tolya199720@gmail.com>
2024-07-27 21:02:20 +0300

    0.1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MyClassLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Tank tank1 = new Tank("T-34");
+            Tank tank2 = new Tank("Pantera");
+
+            Console.WriteLine(tank1.GetTankStatus());
+            Console.WriteLine(tank2.GetTankStatus());
+            string battleResult = tank1 * tank2;
+            Console.WriteLine(battleResult);
+        }
+    }
+}
