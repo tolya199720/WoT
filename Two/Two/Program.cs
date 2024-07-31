99642b0b6d54e3b8a69e748316dbd11523d6ffe2
tolya199720 <tolya199720@gmail.com>
2024-07-31 17:44:41 +0300

    delegate

--- Two/Two/Program.cs
+++ Two/Two/Program.cs
@@ -1,16 +1,164 @@
 ﻿using System;
+using Library1;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using static Two.Program;
 
 namespace Two
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+        public delegate bool Del(object obj1, object obj2);
+        delegate Boolean Filter(int number);
+        //Метод для отримання усіх простих чисел у масиві; 
+        class prime
         {
-            Console.WriteLine("hello two");
+           public object[] qws = { 1, 2, 3, 4, 5, 9, 8, 7, 6, 10 };
+
+            public static bool prim (int number)
+            {
+                if (number <= 1)
+                    return false;
+
+                if (number == 2)
+                    return true;
+
+                if (number % 2 == 0)
+                    return false;
+
+                for (int i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                        return false;
+                }
+
+                return true;
+            }
+        }
+        public void Prime(Filter F, object[] qws)
+        {
+           
+            int count = 0;
+            int[] temp = new int[qws.Length];
+
+            for (int i = 0; i < qws.Length; i++)
+            {
+                if (qws[i] is int number && F(number))
+                {
+                    temp[count++] = number;
+                }
+            }
+
+            Console.WriteLine("Prime numbers:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(temp[i]);
+            }
         }
     }
-}
+
+//    Метод для отримання усіх парних чисел у масиві; 
+//■ Метод для отримання усіх непарних чисел у масиві;
+
+    static class Programs
+        {
+            
+            public static bool CompareOne(object obj1, object obj2)
+            {
+                return (int)obj1 > (int)obj2;
+            }
+
+            public static bool CompareTwo(object obj1, object obj2)
+            {
+                return (int)obj1 < (int)obj2;
+            }
+
+            
+            public static void Addeven(object[] add, Del dg)
+            {
+                for (int i = 0; i < add.Length; i++)
+                {
+                    for (int j = i + 1; j < add.Length; j++) 
+                    {
+                        if (dg(add[i], add[j]))
+                        {
+                            object temp = add[i];
+                            add[i] = add[j];
+                            add[j] = temp;
+                        }
+                    }
+                }
+            }
+        }
+
+//    Завдання 2 Створіть набір методів:
+//■ Метод відображення поточного часу; 
+//■ Метод відображення поточної дати; 
+//■ Метод відображення поточного дня тижня; 
+//■ Метод для підрахунку площі трикутника; 
+//■ Метод для підрахунку площі прямокутника.
+
+    public delegate void DisplayDelegate();
+
+    public delegate double AreaDelegate(double x, double y);
+
+    class Program
+    {
+        public static void ShowCurrentTime()
+        {
+            Console.WriteLine("Current Time: " + DateTime.Now.ToString("HH:mm:ss"));
+        }
+
+        public static void ShowCurrentDate()
+        {
+            Console.WriteLine("Current Date: " + DateTime.Now.ToString("yyyy-MM-dd"));
+        }
+
+        public static void ShowCurrentDayOfWeek()
+        {
+            Console.WriteLine("Current Day of Week: " + DateTime.Now.DayOfWeek);
+        }
+
+        public static double CalculateTriangleArea(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+
+        public static double CalculateRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            object[] qws = { 1, 2, 3, 4, 5, 9, 8, 7, 6, 10 };
+
+            Del d = new Del(Programs.CompareOne);
+            Del dtwo = new Del(Programs.CompareTwo);
+
+            Programs.Addeven(qws, d);
+
+            Console.WriteLine("Sorted using CompareOne:");
+            foreach (object o in qws)
+            {
+                Console.WriteLine(o);
+            }
+
+            Programs.Addeven(qws, dtwo);
+
+            Console.WriteLine("\nSorted using CompareTwo:");
+            foreach (object o in qws)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+
+
+
---
+++ WoT/WoT/WordOfTank/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoT.WordOfTank
+{
+    internal class Tank
+    {
+        
+            string name;
+            Int32 ummunition;
+            Int32 armor;
+            int agile;
+
+            public Tank(string name)
+            {
+                Random r = new Random(DateTime.Now.Millisecond);
+                this.name = name;
+                this.ummunition = r.Next(100);
+                armor = r.Next(100);
+                agile = r.Next(100);
+            }
+            public static string operator *(Tank c1, Tank c2)
+            {
+                int win1 = 0;
+                int win2 = 0;
+                win1 += (c1.agile > c2.agile) ? 1 : 0;
+                win1 += (c1.armor > c2.armor) ? 1 : 0;
+                win1 += (c1.ummunition > c2.ummunition) ? 1 : 0;
+
+                if (win1 == 2)
+                {
+                    return "Win Tank T-34";
+                }
+                else
+                {
+                    return "Win Tank Pantera";
+                }
+            }
+
+        
+    }
+}
